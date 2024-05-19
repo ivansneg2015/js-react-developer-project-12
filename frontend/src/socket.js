380db9a9d03b308f6d18a32b9919cb3938c325ec
ivansneg2015 <sneg.ivan2015@yandex.ru>
2024-05-19 15:44:39 +0300

    socket

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,9 +1,8 @@
 import { io } from 'socket.io-client';
 
-// Check if process.env.NODE_ENV exists to avoid errors
+// "undefined" means the URL will be computed from the `window.location` object
 const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:3000';
 
-// Create socket connection with computed URL
-const socket = io(URL); // "undefined" means the URL will be computed from the `window.location` object
+const socket = io(URL);
 
 export default socket;

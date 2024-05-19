4f39bc33b59a1640b69e532f972759400d875e51
ivansneg2015 <sneg.ivan2015@yandex.ru>
2024-05-19 03:23:39 +0300

    chanel

--- frontend/src/components/chatComponents/modals/RenameChannel.jsx
+++ frontend/src/components/chatComponents/modals/RenameChannel.jsx
@@ -32,6 +32,8 @@ const RenameChannelComponent = () => {
 
   const channelsNames = channels.data.map((channel) => channel.name);
   const currentChannelName = selectedChannel.name;
+  // Логирование значения currentChannelName для проверки
+  console.log('Current Channel Name:', currentChannelName);
 
   const formik = useFormik({
     initialValues: {

03dd66578da45f15a2c0f5538868e2329dd5cd73
ivansneg2015 <sneg.ivan2015@yandex.ru>
2024-05-29 22:19:58 +0300

    filter

--- frontend/src/utils/FilterProvider.js
+++ frontend/src/utils/FilterProvider.js
@@ -1,10 +1,10 @@
-import React, { createContext, useContext } from 'react';
+import React, { createContext, useContext, useCallback } from 'react';
 import leoProfanity from 'leo-profanity';
 
 const FilterContext = createContext();
 
 export const FilterProvider = ({ children }) => {
-  const filter = (text) => leoProfanity.clean(text);
+  const filter = useCallback((text) => leoProfanity.clean(text), []);
 
   return (
     <FilterContext.Provider value={filter}>
@@ -14,4 +14,3 @@ export const FilterProvider = ({ children }) => {
 };
 
 export const useFilter = () => useContext(FilterContext);
-

5a84fdb821c76187363109c113af51f27779ece6
ivansneg2015 <sneg.ivan2015@yandex.ru>
2024-05-29 21:12:15 +0300

    notfoundpage

--- frontend/src/components/NotFoundPage.jsx
+++ frontend/src/components/NotFoundPage.jsx
@@ -1,18 +1,16 @@
 import { useTranslation } from 'react-i18next';
-import img from '../assets/404.jpg';
-import getPath from '../routes.js';
-
+import notFoundImage from '../assets/404.jpg';
 const NotFoundPage = () => {
   const { t } = useTranslation();
 
   return (
     <div className="text-center">
-      <img alt={t('mainComponents.notFound')} className="img-fluid" src={img} />
+      <img alt={t('mainComponents.notFound')} className="img-fluid" src={notFoundImage} />
       <h1 className="h4 text-muted">{t('mainComponents.notFound')}</h1>
       <p className="text-muted">
         {t('mainComponents.but')}
         {' '}
-        <a href={getPath.chatPage()}>{t('mainComponents.toMainPage')}</a>
+        <a href="/">{t('mainComponents.toMainPage')}</a> {`./App.jsx`}
       </p>
     </div>
   );

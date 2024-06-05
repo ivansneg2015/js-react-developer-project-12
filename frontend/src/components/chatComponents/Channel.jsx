11e750b3ae9c818d58a52e4c3865640fbd157300
ivansneg2015 <sneg.ivan2015@yandex.ru>
2024-06-05 14:03:21 +0300

    channel

--- frontend/src/components/chatComponents/Channel.jsx
+++ frontend/src/components/chatComponents/Channel.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Dropdown, ButtonGroup } from 'react-bootstrap';
 import { useDispatch } from 'react-redux';
 import { useTranslation } from 'react-i18next';
@@ -19,10 +20,18 @@ const Channel = ({ data }) => {
     messageEnd.scrollIntoView();
   };
 
+  const buttonClass = Number(id) !== selectedChannel.currentChannelId
+    ? 'w-100 rounded-0 text-start btn'
+    : 'w-100 rounded-0 text-start btn btn-secondary';
+
   if (!removable) {
     return (
       <li id={id} className="nav-item w-100">
-        <button onClick={() => selectChannel(data)} type="button" className={Number(id) !== selectedChannel.currentChannelId ? 'w-100 rounded-0 text-start btn' : 'w-100 rounded-0 text-start btn btn-secondary'}>
+        <button
+          onClick={() => selectChannel(data)}
+          type="button"
+          className={buttonClass}
+        >
           <span className="me-1">#</span>
           {name}
         </button>
@@ -30,22 +39,39 @@ const Channel = ({ data }) => {
     );
   }
 
+  const dropdownToggleClass = Number(id) !== selectedChannel.currentChannelId
+    ? 'light'
+    : 'secondary';
+
   return (
     <li id={id} className="nav-item w-100">
       {getModalComponent(modal.type)}
       <Dropdown className="d-flex btn-group" as={ButtonGroup}>
-        <button onClick={() => selectChannel(data)} className={Number(id) !== selectedChannel.currentChannelId ? 'w-100 rounded-0 text-start text-truncate btn' : 'w-100 rounded-0 text-start text-truncate btn btn-secondary'} type="button">
+        <button
+          onClick={() => selectChannel(data)}
+          type="button"
+          className={`${buttonClass} text-truncate`}
+        >
           <span className="me-1">#</span>
           {name}
         </button>
-        <Dropdown.Toggle variant={Number(id) !== selectedChannel.currentChannelId ? 'light' : 'secondary'} className="flex-grow-0 dropdown-toggle-split">
-          <span className="visually-hidden">{t('chatComponents.channelControls')}</span>
+        <Dropdown.Toggle
+          variant={dropdownToggleClass}
+          className="flex-grow-0 dropdown-toggle-split"
+        >
+          <span className="visually-hidden">
+            {t('chatComponents.channelControls')}
+          </span>
         </Dropdown.Toggle>
         <Dropdown.Menu>
-          <Dropdown.Item onClick={() => dispatch(openModal({ type: 'removeChannel', id }))}>
+          <Dropdown.Item
+            onClick={() => dispatch(openModal({ type: 'removeChannel', id }))}
+          >
             {t('delete')}
           </Dropdown.Item>
-          <Dropdown.Item onClick={() => dispatch(openModal({ type: 'renameChannel', id }))}>
+          <Dropdown.Item
+            onClick={() => dispatch(openModal({ type: 'renameChannel', id }))}
+          >
             {t('rename')}
           </Dropdown.Item>
         </Dropdown.Menu>
